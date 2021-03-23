4cf6acafdfd2e0d18d8e7753fa7cce1fc47ccb6b
paolotrimini <paolo.trimini@gmail.com>
2021-03-23 15:24:00 +0100

    layout pagina

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+// Sfruttando quando visto questa mattina a lezione, riprodurre
+// la dropdown (menu in alto) che trovate al sito di Revolut.
+
+
+
+
+
+function init() {
+
+}
+
+$(document).ready(init);
