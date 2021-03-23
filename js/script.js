8df8a2a00df32a3196322747230306f6d0f2ca2e
paolotrimini <paolo.trimini@gmail.com>
2021-03-23 15:41:51 +0100

    dropdown menu

--- js/script.js
+++ js/script.js
@@ -3,11 +3,21 @@
 // la dropdown (menu in alto) che trovate al sito di Revolut.
 
 
+function dropdownRevolut() {
+
+    $('.dropdown-item').hover( function () {
+            $('.dropdown-menu', this).slideDown();},
+
+        function () {
+            $('.dropdown-menu', this).slideUp();
+        }
+    );
+};
 
 
 
 function init() {
 
+    dropdownRevolut();
 }
-
 $(document).ready(init);
